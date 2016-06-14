920b967c9f3180992d43a1e9fc9f9fe337b8d52e
Luciano Porto <luciano.bapo@gmail.com>
2016-06-14 03:36:38 -0300

    first commit

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Intervention\Image\ImageManager;
+//use Intervention\Image\Facades\Image as Image;
+
+class ImageController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function file($id, Request $request){
+//        dd($request->all()['file']);
+        $source = 'https://graph.facebook.com/'.$id.'/picture?type=large';
+        // create an image manager instance with favored driver
+        $manager = new ImageManager(array('driver' => 'imagick','allow_url_fopen'=>true));
+
+        // to finally create image instances
+        $image = $manager->make($source);
+        $background = $manager->make($request->all()['file']);
+        $background->insert($image, 'center');
+
+//        $image = \Image::make($source);
+        return $background->response();
+//        dd($source);
+    }
+}
