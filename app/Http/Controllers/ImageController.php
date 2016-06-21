585bf5ea214c443b7c4afeac958ee6e38342a9c9
Luciano Porto <luciano.bapo@gmail.com>
2016-06-20 23:44:49 -0300

    first commit

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -72,7 +72,7 @@ class ImageController extends Controller
         $background = $manager->make($md5['file']);
         $background->insert($image, $position, $x, $y);
 
-        return $background->response();
+        return $background->response('png',60);
     }
 
     public function page(Request $request){
