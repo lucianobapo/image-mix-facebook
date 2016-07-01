1130fb772522096a2f0c9e84804db873b70f163a
Luciano Porto <luciano.bapo@gmail.com>
2016-07-01 08:19:11 -0300

    first commit

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,6 +1,7 @@
 <html>
     <head>
-        <meta property="fb:app_id" content="{{ $app_id }}" />
+        {{--<meta property="fb:app_id" content="{{ $app_id }}" />--}}
+        <meta property="fb:app_id" content="1065386780209103" />
 
         {{--<meta name="twitter:card" content="summary_large_image" />--}}
         {{--<meta name="twitter:site" content="@DeliveryLan" />--}}
@@ -17,6 +18,19 @@
         {{--<meta property="og:image:type" content="image/png"/>--}}
         {{--<meta property="og:description" content="{{ trans('delivery.head.metaDescription') }}"/>--}}
         {{--<meta property="og:updated_time" content="{{ Carbon\Carbon::now()->timestamp }}"/>--}}
+
+        <meta property="og:locale" content="pt_BR" />
+
+        <meta property="og:title" content="Qual jogador famoso é louco por você?" />
+        <meta property="og:description" content="Grandes astros do mundo do futebol agora estão brigando para ver quem conquista você. Faça o teste agora e veja quem é o jogador que é louco por você" />
+        <meta property="og:url" content="http://testesdivertidos-com.umbler.net/qual-jogador-famoso-e-louco-por-voce/" />
+        <meta property="og:site_name" content="Testes Divertidos" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:description" content="Grandes astros do mundo do futebol agora estão brigando para ver quem conquista você. Faça o teste agora e veja quem é o jogador que é louco por você" />
+        <meta name="twitter:title" content="Qual jogador famoso é louco por você?" />
+        <meta name="twitter:image" content="{!! $url !!}" />
+
         <title>{{ $title }}</title>
     </head>
     <body>
