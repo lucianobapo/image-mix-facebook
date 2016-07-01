5eae90cf6e24f06b28f5720e0b4bc8d7573f919c
Luciano Porto <luciano.bapo@gmail.com>
2016-07-01 09:38:22 -0300

    first commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,19 +11,11 @@
 |
 */
 
-$app->get('/{post}', function ($post) use ($app) {
+$app->get('/redirect/{post}', function ($post) {
     $sitename = DB::select('select * from wp_options where `option_name` LIKE \'siteurl\'');
-    dd($sitename);
-    foreach ($sitename as $item) {
-        if ($item->meta_key=="_yoast_wpseo_opengraph-description") $meta['description'] = $item->meta_value;
-        if ($item->meta_key=="_yoast_wpseo_opengraph-title") $meta['title'] = $item->meta_value;
-    }
-    return Redirect::to(''.$post);
-//    dd($post);
+    return Redirect::to($sitename[0]->option_value.'/'.$post);
 });
 $app->get('/', function () use ($app) {
-    $sitename = DB::select('select * from wp_options where `option_name` LIKE \'siteurl\'');
-    dd($sitename);
     return $app->version();
 });
 $app->get('/phpinfo', function () use ($app) {
